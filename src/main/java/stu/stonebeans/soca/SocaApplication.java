1488697855d4fa2f8510ed340043b68278d16583
yeeun <charm5231@naver.com>
2021-01-31 22:03:11 +0900

    2021.01.31 PYE : init prjoect

---
+++ src/main/java/stu/stonebeans/soca/SocaApplication.java
@@ -0,0 +1,13 @@
+package stu.stonebeans.soca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stu/stonebeans/soca/SocaApplicationTests.java
@@ -0,0 +1,13 @@
+package stu.stonebeans.soca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
