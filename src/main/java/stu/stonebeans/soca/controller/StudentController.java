992230a12aa0b7e21704117abda7d36f714d03b4
yeeun <charm5231@naver.com>
2021-02-09 23:19:13 +0900

    2021.02.09 PYE : add resultVO (status, msg), status(-1:fail, 1:success)

--- src/main/java/stu/stonebeans/soca/controller/StudentController.java
+++ src/main/java/stu/stonebeans/soca/controller/StudentController.java
@@ -3,6 +3,7 @@ package stu.stonebeans.soca.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import stu.stonebeans.soca.sbo.StudentSBO;
+import stu.stonebeans.soca.vo.ResultVO;
 import stu.stonebeans.soca.vo.StudentVO;
 
 import java.util.HashMap;
@@ -18,10 +19,19 @@ public class StudentController {
         this.studentSBO = studentSBO;
     }
 
-    // 이메일 중복 여부 체크
-    @RequestMapping(value = "/checkDuplicateEmail", method = RequestMethod.POST)
-    public boolean checkDuplicateEmail(@RequestBody HashMap<String, String> map) {
-        return studentSBO.checkDuplicateEmail(map.get("email"));
+    // 이메일 중복 여부 확인 및 인증 번호 전송
+    @RequestMapping(value = "/sendAuthEmail", method = RequestMethod.POST)
+    public ResultVO sendAuthEmail(@RequestBody HashMap<String, String> map) {
+        ResultVO resultVO = new ResultVO();
+        if(studentSBO.checkDuplicateEmail(map.get("email")) == true) {
+            // 인증 번호 전송
+            resultVO.setStatus(1);
+            resultVO.setMsg("인증번호 전송이 완료되었습니다.");
+        } else {
+            resultVO.setStatus(-1);
+            resultVO.setMsg("이미 사용 중인 이메일 입니다.");
+        }
+        return resultVO;
     }
 
     // 닉네임 중복 여부 체크
