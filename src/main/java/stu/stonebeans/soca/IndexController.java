98c917bbda09fe9dc522ae589e7c9ad651c315bf
yeeun <charm5231@naver.com>
2021-02-11 18:48:40 +0900

    2021.02.11 PYE : add MyPage

--- src/main/java/stu/stonebeans/soca/IndexController.java
+++ src/main/java/stu/stonebeans/soca/IndexController.java
@@ -4,9 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class IndexController {
+
+    // 메인 페이지 이동
     @RequestMapping("/")
     public String home() {
         return "index";
+    }
+
+    // 마이 페이지 이동
+    @RequestMapping(value = "/myPage")
+    public String myPage() {
+        return "myPage";
     }
 }
