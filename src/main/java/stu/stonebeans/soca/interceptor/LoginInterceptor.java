d75094074012bde33ad79ba5a03d27ce3b1a5f35
seongbins <qkrtjdqls031@naver.com>
2021-02-13 14:47:41 +0900

    2021.02.13 PSB : resolve conflict

---
+++ src/main/java/stu/stonebeans/soca/interceptor/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package stu.stonebeans.soca.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    public List includePath = Arrays.asList("/myPage", "/schedule");
+    public List excludePath = Arrays.asList("/css/**", "/js/**", "/images/**", "/", "/index");
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object email = session.getAttribute("email");
+
+        if(email == null) {
+            response.sendRedirect("/noAuth");
+            return false;
+        }
+        return true;
+    }
+
+}
