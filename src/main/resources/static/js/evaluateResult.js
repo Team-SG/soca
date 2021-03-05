8fe4728e6054e1dee28c14f0e3f70a17652340cd
kdb <dongbin97@naver.com>
2021-03-05 12:01:00 +0900

    2021.03.05 KDB : 평가 결과 확인 작성 중

--- src/main/resources/static/js/evaluateResult.js
+++ src/main/resources/static/js/evaluateResult.js
@@ -34,6 +34,21 @@ function initEvaluateResult(){
     callPostService("/getEvaluateData",param2,function(data){
        evaluateResult=data;
     });
-    swal(evaluateResult.evaluationCnt+"");
 
+    for(var i=0;i<evaluateResult.evaluationAvg.toFixed(0);i++)
+        $("#evaluation .star").eq(i).addClass("on");
+    $("#evaluation").append('<h6> ( '+(evaluateResult.evaluationAvg/2).toFixed(2)+ ' / 5.0 )'+tabChar()+' ( '+evaluateResult.evaluationCnt+'명 참여 ) </h6>');
+
+    for(var i=0;i<evaluateResult.qualityAvg.toFixed(0);i++)
+        $("#quality .star").eq(i).addClass("on");
+    $("#quality").append('<h6> ( '+(evaluateResult.qualityAvg/2).toFixed(2)+ ' / 5.0 ) </h6>');
+
+    for(var i=0;i<evaluateResult.gradeSatisAvg.toFixed(0);i++)
+        $("#gradeSatis .star").eq(i).addClass("on");
+    $("#gradeSatis").append('<h6> ( '+(evaluateResult.gradeSatisAvg/2).toFixed(2)+ ' / 5.0 ) </h6>');
+
+
+    $("#difficulty").append('<h6> ( '+evaluateResult.difficultyAvg.toFixed(2)+ ' / 5.0 ) </h6>');
+    $("#homework").append('<h6>( '+evaluateResult.homeworkAvg.toFixed(2)+ ' / 5.0 ) </h6>');
+    $("#coverage").append('<h6>( '+evaluateResult.coverageAvg.toFixed(2)+ ' / 5.0 ) </h6>');
 }
