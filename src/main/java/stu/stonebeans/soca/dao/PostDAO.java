35060b24f807d0ac403631df95d6fa7d809eadc1
seongbins <qkrtjdqls031@naver.com>
2021-08-04 14:31:24 +0900

    2021.08.04 PSB : Merge

--- src/main/java/stu/stonebeans/soca/dao/PostDAO.java
+++ src/main/java/stu/stonebeans/soca/dao/PostDAO.java
@@ -3,9 +3,17 @@ package stu.stonebeans.soca.dao;
 import org.apache.ibatis.annotations.Mapper;
 import stu.stonebeans.soca.vo.PostVO;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Mapper
 public interface PostDAO {
     List<PostVO> getAllPosts();
+    List<PostVO> getUnsolvedPosts();
+
+    // 해당 postNum의 게시글을 불러옴
+    PostVO getPostByNum(int postNum);
+
+    // 해당 subjectID의 과목이름을 불러옴
+    String getSubjectName(HashMap<String, String> map);
 }
--- src/main/java/stu/stonebeans/soca/sbo/PostSBO.java
+++ src/main/java/stu/stonebeans/soca/sbo/PostSBO.java
@@ -2,8 +2,15 @@ package stu.stonebeans.soca.sbo;
 
 import stu.stonebeans.soca.vo.PostVO;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface PostSBO {
-    List<PostVO> getAllPosts();
+    List<PostVO> getAllPosts(Object checked);
+
+    // 해당 postNum의 게시글을 불러옴
+    PostVO getPostByNum(int postNum);
+
+    // 해당 subjectID의 과목이름을 불러옴
+    String getSubjectName(HashMap<String, String> map);
 }
