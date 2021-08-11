cbd6fac9a64a09010a4b3bbe6d7d722e62381b9a
seongbins <qkrtjdqls031@naver.com>
2021-08-11 23:59:10 +0900

    2021.08.11 PSB : autoComplete + grid 데이터 추가.. 이거 autoComplete가 맞을까 싶은 고민

--- src/main/resources/static/js/quickMenuList.js
+++ src/main/resources/static/js/quickMenuList.js
@@ -1,10 +1,39 @@
 const Grid = tui.Grid;
 let gridData;
+var selectItem;
+var subjectLists = [];
 
 $(document).ready(function() {
     $("#quick").load("/quickMenu");
     initGrid();
     getAllMajors();
+    getAllSubjects();
+    autoComplete(1);
+    $("input[name='searchCondition']").change(function() {
+        gridData.clear();
+        $("#subject").val("");
+        $("#selectMajor").val("전공");
+        if($("input[id='courseNum']:checked").prop("checked")) {
+            $("#major").show();
+            $("#hideSubjectLists").empty();
+            $("#subjectLists").hide();
+            // 과목번호 autocomplete
+            autoComplete(1);
+        } else if($("input[id='courseName']:checked").prop("checked")) {
+            $("#major").show();
+            $("#hideSubjectLists").empty();
+            $("#subjectLists").hide();
+            // 과목명 autocomplete
+            autoComplete(2);
+        }
+    })
+
+    $("#btnSearch").click(function(){
+        gridData.clear();
+        showSearchData();
+        $("#subjectLists").show(); //slideUp slideDown..
+    });
+
 })
 
 
@@ -32,23 +61,18 @@ function initGrid() {
                 header: '학과',
                 width: 'auto',
                 minWidth: '230',
+                align: 'center',
                 name: 'major',
             },
             {
                 header: '과목',
                 width: 'auto',
                 minWidth: '250',
+                align: 'center',
                 name: 'subjectNO'
             }
         ]
     });
-
-    gridData.on('click', (ev) => {
-        if(ev.columnName === "subjectNO") {
-            if(gridData.getRow(ev.rowKey).professor != null)
-                goSelected(gridData.getRow(ev.rowKey));
-        }
-    })
 
     gridData.resetData(data);
 }
@@ -63,5 +87,69 @@ function callGetAllMajors(data) {
     $.each(data, function(index, item) {
         var option = "<option value='" + item.major + "'>"+ item.major + "</option>";
         $("#selectMajor").append(option);
+    });
+}
+
+function getAllSubjects() {
+    callPostService("getAllSubjects", 1, function (data) {
+        subjectLists = data;
+    })
+}
+
+function autoComplete(num) {
+    var autoData = subjectLists;
+
+    $("#subject").autocomplete({
+        source : function(request, response) {
+            var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(request.term), "i");
+            response($.map(autoData, function(item) {
+                var testVal;
+                if(num == 1) {
+                    testVal = item.code;
+                } else if(num == 2) {
+                    testVal = item.subjectNO;
+                }
+                if (matcher.test(testVal)) {
+                    var major = $("#selectMajor").val();
+                    var result = {
+                        label: item.subjectNO + "[" + item.code + "]",
+                        value: item.subjectNO + item.code,
+                        code: item.code,
+                        major: item.major,
+                        subjectNO: item.subjectNO,
+                        num : 1
+                    }
+                    if(major == "전공") {
+                        return result;
+                    } else {
+                        if(major == item.major) {
+                            return result;
+                        }
+                    }
+                }
+            }));
+        },
+        select : function(event, ui) {
+            event.preventDefault();
+            $("#subject").val(ui.item.label);
+            selectItem = ui.item;
+        }
+    });
+}
+
+function showSearchData() {
+    //$("#subjectLists").cleanData();
+    //$("#subjectLists").append(selectItem.label);
+    $("#hideSubjectLists").empty();
+    $("#menuSubjectLists").attr('alt', selectItem.label);
+    var rowData = [];
+    var child = {
+            code: selectItem.code,
+            major: selectItem.major,
+            subjectNO: selectItem.subjectNO,
+    }
+    rowData.push(child);
+    rowData.forEach(row => {
+        gridData.appendRow(row);
     });
 }
