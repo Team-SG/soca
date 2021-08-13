2e8e67f6cd64622c8281825b593da2b3bcf097a3
kdb <dongbin97@naver.com>
2021-08-13 11:02:54 +0900

    21.08.13 KDB: 질문 읽기 - 대댓글 달기

--- src/main/resources/static/js/postRead.js
+++ src/main/resources/static/js/postRead.js
@@ -5,6 +5,7 @@
  */
 
 var postNum;
+var replyIdx = 0;
 
 $(document).ready(function(){
     initPostRead();
@@ -31,13 +32,12 @@ function writeReply(){
             postNum : postNum,
             content : $("#replyContent").val()
         }
-        callPostService("/writeReply",param, function(){
-            location.reload();
-        })
+        callPostService("/writeReply",param, null);
+        location.reload();
     }
 }
 
-function replyClick(id, rereplyCnt){
+function replyClick(id, index, offset){
     $("#writeRereply").remove();
     var replyNum=id.substring(12);
     var text = '<li id="writeRereply" class="list-group-item d-flex justify-content-between align-items-center">'
@@ -48,8 +48,7 @@ function replyClick(id, rereplyCnt){
                 + '<button type="button" id="btnRereplyWrite" class="btn btn-primary font-weight-normal ml-2">작성</button>'
                 + '</li>';
 
-    if(rereplyCnt==0) $("#reply"+replyNum).after(text);
-    else $("#rereply"+replyNum).after(text);
+    $("#"+(index+offset)).after(text);
 
     $("#btnRereplyWrite").click(function(){
         if($("#rereplyContent").val().length==0){
@@ -82,8 +81,8 @@ function callGetPostByNum(data){
 
 function callGetReplies(reply){
     for(var i=0;i<reply.length;i++){
-        var text = '<li id="reply' + reply[i].replyNum + '" class="list-group-item d-flex justify-content-between align-items-center">';
-        text += '<div>' + reply[i].content + '</div>';
+        var text = '<li id="' + (++replyIdx) + '" class="list-group-item d-flex justify-content-between align-items-center">';
+        text += '<div>' +replyIdx+' '+ reply[i].content + '</div>';
         text += '<div class="d-flex justify-content-center align-items-center">';
         text += '<div class="mr-1"> ['+ reply[i].nickname + '] </div>';
         text += '<div> ['+ reply[i].postTime + '] </div>';
@@ -93,8 +92,8 @@ function callGetReplies(reply){
                 + '<path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>'
                 + '</svg></a>';
         text += '<div class="dropdown-menu" style="text-align: center; min-width: 5rem;">'
-                + '<a id="writeRereply' + reply[i].replyNum + '" class="dropdown-item fs-090" onClick="replyClick(this.id,' + reply[i].rereplyCnt + ')">답글 달기</a>'
-                + '<a id="accuse' + i + '" class="dropdown-item fs-090">신고하기</a>'
+                + '<a id="writeRereply' + reply[i].replyNum + '" class="dropdown-item fs-090" onClick="replyClick(this.id,' + replyIdx+','+reply[i].rereplyCnt + ')">답글 달기</a>'
+                + '<a class="dropdown-item fs-090">신고하기</a>'
                 + '</div>';
         text += '</div></div></li>';
 
@@ -105,14 +104,15 @@ function callGetReplies(reply){
 
 }
 function callGetRereplies(rereply){
-    var text = '<div id="rereply' + rereply[0].replyNum + '">';
-    for(var i=0;i<rereply.length;i++){
-        text += '<li id="rereply' + rereply[i].rereplyNum + '" class="list-group-item d-flex justify-content-between align-items-center">'
+    var start = replyIdx;
+    var text = '';
+    for(var i = 0;i < rereply.length;i++){
+        text += '<li id="' + (++replyIdx) + '" class="list-group-item d-flex justify-content-between align-items-center">'
                 + '<div class="d-flex justify-content-center align-items-center">'
                 + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right mr-2" viewBox="0 0 16 16">'
                 + '<path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"/>'
                 + '</svg>'
-                + '<div>' + rereply[i].content + '</div></div>'
+                + '<div>' + replyIdx+' '+rereply[i].content + ' '+rereply[i].rereplyNum+ '</div></div>'
                 + '<div class="d-flex justify-content-center align-items-center">'
                 + '<div class="mr-1"> ['+ rereply[i].nickname + '] </div>'
                 + '<div> ['+ rereply[i].postTime + '] </div>'
@@ -122,10 +122,9 @@ function callGetRereplies(rereply){
                 + '<path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>'
                 + '</svg></a>'
                 + '<div class="dropdown-menu" style="text-align: center; min-width: 5rem;">'
-                + '<a id="accuse' + i + '" class="dropdown-item fs-090">신고하기</a>'
+                + '<a class="dropdown-item fs-090">신고하기</a>'
                 + '</div>'
                 + '</div></div></li>';
     }
-    text += '</div>';
-    $("#reply"+rereply[0].replyNum).after(text);
+    $("#"+start).after(text);
 }
